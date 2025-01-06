d9d2db302e07ce8fcaf70ef5034a3e9252567ceb
Beth Delaney <64157328+bethdelaney@users.noreply.github.com>
2025-01-06 17:04:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='soil-python',
-    version='0.4',
+    version='0.6',
     packages=find_packages(),
     install_requires=[
         'numpy',
