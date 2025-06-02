23f69c2d16e1e0a16925e73c54e365313cf99fa3
Beth Delaney <64157328+bethdelaney@users.noreply.github.com>
2025-06-02 12:12:24 +0200

    Update __init__.py

--- soil_python/hyperspectral/__init__.py
+++ soil_python/hyperspectral/__init__.py
@@ -1,1 +1,1 @@
-from .convolution import convolve_to_hyperion, convolve_to_enmap, convolve_to_landsatnext, convolve_to_s2
+from .convolution import convolve_to_hyperion, convolve_to_enmap, convolve_to_landsatnext, convolve_to_s2, convolve_to_landsat8
