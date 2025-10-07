cc841ba37696be625ca951d6811d45cb27211e32
Beth Delaney <64157328+bethdelaney@users.noreply.github.com>
2025-10-07 16:49:30 +0200

    Import additional convolution functions from gaussian_convolution

--- soil_python/hyperspectral/__init__.py
+++ soil_python/hyperspectral/__init__.py
@@ -1,1 +1,2 @@
 from .convolution import convolve_to_hyperion, convolve_to_enmap, convolve_to_landsatnext, convolve_to_s2, convolve_to_landsat8
+from .gaussian_convolution import convolve_to_enmap, convolve_to_landsatnext, convolve_to_landsat8
